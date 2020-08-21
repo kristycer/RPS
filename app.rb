ef20899fa6d62653e3231d68a86d6c7c54beae52
kristyna cervenkova <kristyna275@gmail.com>
2020-08-21 07:08:29 +0200

    user chosen weapon presented

--- app.rb
+++ app.rb
@@ -14,9 +14,14 @@ class RPS < Sinatra::Base
 
   get '/play' do
     @name = session[:name]
+    @weapon = session[:weapon]
     erb :play
   end
 
+  post '/play' do
+    session[:weapon] = params[:weapon]
+    redirect '/play'
+  end 
   
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -12,8 +12,13 @@ feature 'play a game' do
         click_button 'Submit'
     end 
     scenario 'see the choices' do
-        expect(page).to have_content 'Rock'
-        expect(page).to have_content 'Paper'
-        expect(page).to have_content 'Scissors'
+        expect(page).to have_button 'Rock'
+        expect(page).to have_button 'Paper'
+        expect(page).to have_button 'Scissors'
+    end 
+
+    scenario 'clicks on the option' do
+        click_button 'Rock'
+        expect(page).to have_content 'Your weapon is Rock'
     end 
 end 
